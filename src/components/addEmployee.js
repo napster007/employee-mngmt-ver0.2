072de5a019bbad705b4cf749be622f2b2f74c229
Jayson Olaguera <napsterbond@yahoo.com>
2020-12-29 02:19:10 +0800

    set admin nd employee

--- src/components/addEmployee.js
+++ src/components/addEmployee.js
@@ -6,7 +6,8 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Input from '@material-ui/core/Input';
-
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 import app from '../firebase'; 
 
 
@@ -22,6 +23,7 @@ class AddEmployee extends Component {
       cpnum:'',
       address:'',
       password:'',
+      utype:'',
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -36,7 +38,47 @@ class AddEmployee extends Component {
   handleSubmit(e) {
     e.preventDefault();
     const itemsRef = app.database().ref('users');
+    var fname = this.state.fname;
+     var mname= this.state.mname;
+      var lname= this.state.lname;
+     var bday= this.state.bday;
+      var email= this.state.email;
+      var cpnum=this.state.cpnum;
+      var address=this.state.address;
+      var password=this.state.password;
+      var utype= this.state.utype;
     const item = {
+      email: this.state.email,
+      password:this.state.password,
+    }
+     app.auth().createUserWithEmailAndPassword(item.email,item.password).then(cred => {
+      //console.log(cred.user.uid);
+      var useruId = cred.user.uid;
+      logUser(useruId); 
+       
+ 
+   
+    });
+
+
+function logUser(useruId) {
+    var ref = app.database().ref("users");
+    var obj = {
+        fname:fname,
+      mname: mname,
+      lname: lname,
+      bday: bday,
+      email: email,
+      cpnum:cpnum,
+      address:address,
+      password:password,
+      uid:useruId,
+      utype:utype,
+    };
+    ref.push(obj);
+}
+ 
+    /*  const item2 = {
       fname: this.state.fname,
       mname: this.state.mname,
       lname: this.state.lname,
@@ -44,14 +86,11 @@ class AddEmployee extends Component {
       email: this.state.email,
       cpnum:this.state.cpnum,
       address:this.state.address,
-      password:this.state.password
+      password:this.state.password,
+      uid: this.uid,
       
     }
-    app.auth().createUserWithEmailAndPassword(item.email,item.password).then(cred => {
-      console.log(cred.uid);
-
-    });
-    itemsRef.push(item);
+    itemsRef.push(item2); */
     this.setState({
       fname: '',
       mname:'',
@@ -60,10 +99,15 @@ class AddEmployee extends Component {
       email: '',
       cpnum:'',
       address:'',
-      password:''
+      password:'',
+      uid:'',
+      utype:'',
     });
   }
+
+  
   render() {
+    
     return (
       <div className='add-form-wrapper'>
         
@@ -100,8 +144,23 @@ class AddEmployee extends Component {
   
               <Input className="inputDatas" required id="standard-required" placeholder="Address" name="address" onChange={this.handleChange} value={this.state.address} />
                <Divider/>
+                <Divider/>
+                 <h4>User Type</h4>
+                   
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                name="utype"
+                value={this.state.utype}
+                onChange={this.handleChange}
+              >
+                <MenuItem value={"admin"}>ADMIN</MenuItem>
+                <MenuItem value={"employee"}>EMPLOYEE</MenuItem>
+                
+              </Select>
+                <Divider/>
                <h4>User Credentials</h4>
-               
+              
                
                <Input className="inputDatas" required id="standard-required" autoComplete="off" placeholder="Email" name="email" onChange={this.handleChange} value={this.state.email} />
               
